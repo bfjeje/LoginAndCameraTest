9c1f92140eff9ad543fc8677f2cbae149ed20d6e
bruno.ellerbach <ingbrunofernandez@gmail.com>
2019-09-20 11:01:58 -0300

    [Feature]Added arrow to point to the fab. Text modified to explain the function of the fab.

--- app/src/main/java/bruno/fernandez/loginandcameratest/ui/home/HomeViewModel.java
+++ app/src/main/java/bruno/fernandez/loginandcameratest/ui/home/HomeViewModel.java
@@ -10,7 +10,7 @@ public class HomeViewModel extends ViewModel {
 
     public HomeViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is home fragment");
+        mText.setValue("Clique no ícone abaixo para tirar uma nova foto.");
     }
 
     public LiveData<String> getText() {
